4e1e5b9158a0b39f37d1ef1b4655aee8e7bde1a6
Raphael <raphaelcamar@outlook.com>
2022-10-01 16:59:09 -0300

    seção 4: plugins

---
+++ plugins/src/index.js
@@ -0,0 +1,9 @@
+import Heading from './components/heading/heading.js'
+
+const heading = new Heading()
+
+heading.create("Página principal")
+
+console.log(PORT)
+console.log(VERSION)
+console.log(process.env.API_KEY)
